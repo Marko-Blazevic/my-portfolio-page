4561d216fcca508cd284bbfb224faab28eb5010d
Marko Blazevic <www.markoblazevic@gmail.com>
2020-07-06 14:31:25 -0300

    changed layout

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const welcomeStart = document.querySelector('.welcome-start');
+const header = document.getElementById('my-header');
+const welcomeBox = document.querySelector('.welcome-box');
 const scrollEl = document.querySelector('.scroll');
 
 window.addEventListener('scroll', () => {
-  scrollEl.classList.toggle('scroll-on');
+  const x = window.scrollY;
+  console.log(x);
+
+  scrollEl.style.opacity = Math.max(0, 1 - x / 100);
+  welcomeBox.style.opacity = Math.max(0, 1 - x / 400);
+
+  if (x > 800) {
+    header.style.transition = 'none';
+    header.style.opacity = 0;
+  } else {
+    header.style.opacity = 1;
+  }
+
+  const contactBox = document.querySelector('.contact-container');
+  if (x > 1600) {
+    contactBox.style.transform = 'scale(1)';
+  }
 });
