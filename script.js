64f7f47ac347f1016c28bd13d5e2e7d2488f2ba5
Marko Blazevic <www.markoblazevic@gmail.com>
2022-06-03 11:35:23 -0300

    Added animation for about

--- script.js
+++ script.js
@@ -1,49 +1,7 @@
 const header = document.querySelector('.my-header');
-// const welcome = document.getElementById('welcome');
-// const about = document.getElementById('about');
-// const scrollEl = document.querySelector('.scroll');
 const welcomeContainer = document.querySelector('.letter-container');
-// const welcomeBackgroundImage = document.querySelector(
-//   '.welcome-background-img'
-// );
 
-// welcomeContainer.children[6].addEventListener('animationend', () => {
-//   for (i = 0; i < 7; i++) {
-//     const letter = welcomeContainer.children[i];
-//     letter.classList.add('little-popup');
-//   }
-//   setTimeout(() => {
-//     welcomeContainer.classList.add('box-shadow');
-//     welcomeContainer.classList.add('letter-container-after');
-//     welcomeBackgroundImage.classList.add('img-opacity');
-//     header.classList.add('header-black');
-//   }, 100);
-// });
-
-// const rollElement = () => {
-//   letter.classList.add('little-popup');
-// };
-// const elementToNormal = () => {
-//   letter.classList.remove('little-popup');
-// };
-
-// const x = window.scrollY;
-// aboutRect = about.getBoundingClientRect();
-// // if (aboutRect.top < (7.5rem))
-
-// window.addEventListener('scroll', () => {
-//   header.classList.add('header-smaller');
-//   welcomeContainer.style.transition = 'none';
-
-//   scrollEl.style.opacity = Math.max(0, 1 - x / 100);
-//   welcomeContainer.style.opacity = Math.max(0, 1 - x / 600);
-
-//   if (x > 800) {
-//     header.classList.add('header-hide');
-//   } else {
-//     header.classList.remove('header-hide');
-//   }
-// });
+gsap.to('.my-header', { duration: 2, ease: 'bounce.out', y: 0, opacity: 1 });
 
 gsap.to('.letter-w', { duration: 1.5, x: '0', opacity: 1 });
 gsap.to('.letter-e1', { duration: 1.5, x: '0', opacity: 1, delay: 0.2 });
@@ -67,11 +25,42 @@ gsap.to('.letter-m', { color: 'white', delay: 3 });
 gsap.to('.letter-e2', { color: 'white', delay: 3.2 });
 
 setTimeout(() => {
-  welcomeContainer.classList.add('letter-container-after');
-  header.classList.add('header-black');
-}, 3000);
-setTimeout(() => {
   header.classList.add('header-black');
 }, 2000);
 
-gsap.to('.my-header', { duration: 2, ease: 'bounce.out', y: 0, opacity: 1 });
+setTimeout(() => {
+  welcomeContainer.classList.add('letter-container-after');
+  header.classList.add('header-black');
+}, 3000);
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.my-picture', {
+  scrollTrigger: '.my-picture',
+  x: -100,
+  opacity: 0.2,
+  duration: 2,
+});
+
+gsap.from('.about-paragraph', {
+  scrollTrigger: '.about-paragraph',
+  y: 100,
+  opacity: 0,
+  duration: 2,
+});
+
+gsap.fromTo(
+  '#cursor',
+  { autoAlpha: 0, x: -20 },
+  { autoAlpha: 1, duration: 0.5, repeat: -1, ease: SteppedEase.config(1) }
+);
+
+let tween = gsap.to('.typewriter', {
+  text: {
+    value:
+      'this is a custom text written to show my easy approaches to make the typewriting easy!',
+  },
+  duration: 5,
+  delay: 1,
+  ease: 'none',
+});
