67538097b37144f1b6318aaf793055bcd2c0b361
Marko Blazevic <www.markoblazevic@gmail.com>
2022-11-21 10:06:07 -0300

    Added animation for pointer and #contact inputs. Reduced css nesting for #about. Changed css for #contact-inputs. Added @media portrait for #welcome.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const header = document.querySelector('.my-header');
 const welcomeContainer = document.querySelector('.letter-container');
 const welcomeLetters = gsap.utils.toArray('.letter');
+const slideUp = gsap.utils.toArray('.slide-up');
+const popUp = gsap.utils.toArray('.pop-up');
+const imageWrapper = document.querySelector('.about-img-wrapper');
+const body = document.querySelector('.main-content');
+
+body.addEventListener('scroll', console.log('it moves'));
+function toggleHeader() {
+  if (imageWrapper.offsetTop < 76) {
+    header.classList.add('un-fixed');
+  } else {
+    header.classList.remove('un-fixed');
+  }
+}
 
 let tl = gsap.timeline();
+
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.to('.my-header', { duration: 2, ease: 'bounce.out', y: 0, opacity: 1 });
 
@@ -12,19 +27,30 @@ welcomeLetters.forEach((elem) => {
 welcomeLetters.forEach((elem) => {
   tl.to(elem, { color: 'white', duration: 0.15 });
 });
-
 tl.to(
   '.welcome-background-img',
   { duration: 1, opacity: 1, ease: 'none' },
   '-=1.5'
 );
-
 setTimeout(() => {
   welcomeContainer.classList.add('letter-container-after');
   header.classList.add('header-black');
 }, 2000);
-
-gsap.registerPlugin(ScrollTrigger);
+tl.from(
+  '.slide-down',
+  {
+    duration: 2,
+    y: -200,
+    opacity: 0,
+  },
+  '<1'
+);
+tl.to('.pulse', {
+  duration: 2,
+  opacity: 0.2,
+  repeat: -1,
+  yoyo: true,
+});
 
 gsap.from('.my-picture', {
   scrollTrigger: '.my-picture',
@@ -32,34 +58,26 @@ gsap.from('.my-picture', {
   opacity: 0.2,
   duration: 2,
 });
-
-const paragraphs = gsap.utils.toArray('.slide-up-p');
-paragraphs.forEach((para) => {
-  gsap.from(para, {
-    y: 100,
+slideUp.forEach((elem) => {
+  gsap.from(elem, {
+    y: 80,
     opacity: 0,
-    duration: 1.5,
-    scrollTrigger: para,
+    duration: 1,
+    scrollTrigger: elem,
   });
 });
 gsap.from('.slide-right', {
   scrollTrigger: '.slide-right',
-  x: -100,
+  x: -80,
   opacity: 0,
-  duration: 1.5,
-});
-
-gsap.from('.pop-up', {
-  scrollTrigger: '.pop-up',
-  duration: 2,
-  scale: 0,
-  opacity: 0.2,
-  ease: Bounce.easeOut,
-});
-
-tl.from('.pointer', {
   duration: 1,
-  opacity: 0,
-  repeat: -1,
-  yoyo: true,
+});
+popUp.forEach((elem) => {
+  gsap.from(elem, {
+    scrollTrigger: elem,
+    duration: 3,
+    scale: 0,
+    opacity: 0.2,
+    ease: 'elastic.out(1, 1)',
+  });
 });
